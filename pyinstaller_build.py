1a5490a4d761a53d91a5af4dab8e7b02a6354717
Amber Sahdev <asahdev2@illinois.edu>
2024-02-27 15:54:28 -0800

    Improved pyisntaller build script

--- pyinstaller_build.py
+++ pyinstaller_build.py
@@ -1,46 +1,66 @@
 """
-Can never get pyisntaller to work
+> python3.9 -m venv venv
+> source venv/bin/activate
+> python3.9 -m pip install -r requirements.txt
+> python3.9 build.py
 
 NOTES:
-    1. extra steps before using multiprocessing might be required https://www.pyinstaller.org/en/stable/common-issues-and-pitfalls.html#why-is-calling-multiprocessing-freeze-support-required
-    2. Change file reads accordingly https://pyinstaller.org/en/stable/runtime-information.html#placing-data-files-at-expected-locations-inside-the-bundle
+    1. extra steps before using multiprocessing might be required
+        https://www.pyinstaller.org/en/stable/common-issues-and-pitfalls.html#why-is-calling-multiprocessing-freeze-support-required
+    2. Change file reads accordingly
+        https://pyinstaller.org/en/stable/runtime-information.html#placing-data-files-at-expected-locations-inside-the-bundle
+    3. Code signing for MacOS
+        https://github.com/pyinstaller/pyinstaller/wiki/Recipe-OSX-Code-Signing
+        https://developer.apple.com/library/archive/technotes/tn2206/_index.html
+        https://gist.github.com/txoof/0636835d3cc65245c6288b2374799c43
+        https://github.com/txoof/codesign
+        https://github.com/The-Nicholas-R-Barrow-Company-LLC/python3-pyinstaller-base-app-codesigning
 """
 
-import PyInstaller.__main__
-import platform
 import os
+import platform
+
+import PyInstaller.__main__
+
 
 def build():
     # Path to your main application script
     app_script = os.path.join('app', 'app.py')
-    print(f'app_script:{app_script}')
 
     # Common PyInstaller options
     pyinstaller_options = [
         '--clean',
         '--noconfirm',
-        '--debug=all',
-        '--onefile',  # NOTE: might not work on windows
+
+        # Debug
+        # '--debug=all',
+
+        # --- Basics --- #
+        '--name=Open Interface',
+        '--icon=app/resources/icon.png',
+        '--onefile',  # NOTE: Might not work on Windows
+        '--windowed',  # Remove this if your application is a console program, also helps to remove this while debugging
+
+        # Where to find necessary packages to bundle (python3 -m pip show xxx)
         '--paths=./venv/lib/python3.9/site-packages',
-        '--paths=/usr/local/lib/python3.9/site-packages',
+
+        # Packaging fails without explicitly including these modules here as shown by the logs outputted by debug=all
         '--hidden-import=pyautogui',
-        '--hidden-import=openai',
         '--hidden-import=appdirs',
         '--hidden-import=pyparsing',
-        '--hidden-import=pyaudio',
+        # NOTE: This is the name of the directory that this package is in within ../site-packages/,
+        # whereas the pypi name is SpeechRecognition (pip install SpeechRecognition).
+        # This was hard to pin down and took a long time to debug.
         '--hidden-import=speech_recognition',
-        # '--windowed',  # Remove this if your application is a console program
-        '--name=Open Interface',
-        '--add-data=app/resources/icon.png:resources',
-        '--add-data=app/resources/microphone.png:resources',
-        '--add-data=app/resources/context.txt:resources',
-        '--add-data=app/core.py:.',
-        '--add-data=app/interpreter.py:.',
-        '--add-data=app/llm.py:.',
-        '--add-data=app/ui.py:.',
-        '--add-data=app/utils/settings.py:utils',
-        '--add-data=app/utils/screen.py:utils',
-        '--add-data=app/utils/local_info.py:utils',
+
+        # Static files and resources --add-data=src:dest
+        # - File reads change accordingly - https://pyinstaller.org/en/stable/runtime-information.html#placing-data-files-at-expected-locations-inside-the-bundle
+        '--add-data=app/resources/*:resources',
+
+        # Manually including source code and submodules because app doesn't launch without it
+        '--add-data=app/*.py:.',
+        '--add-data=app/utils/*.py:utils',  # Submodules need to be included manually
+
         app_script
     ]
 
@@ -64,8 +84,6 @@ def build():
     """
 
     # Run PyInstaller with the specified options
-    #PyInstaller.__main__.run(['-v'])
-    print("\n")
     PyInstaller.__main__.run(pyinstaller_options)
 
 
