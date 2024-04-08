0a8184a378f4cea9958c0fdac098e55af8f780e3
Amber Sahdev <asahdev2@illinois.edu>
2024-04-07 20:32:25 -0700

    Compiling new binaries, improved error reporting

--- app/core.py
+++ app/core.py
@@ -16,10 +16,12 @@ class Core:
         self.settings_dict = Settings().get_dict()
 
         self.interpreter = Interpreter(self.status_queue)
+
+        self.llm = None
         try:
             self.llm = LLM()
         except OpenAIError as _:
-            self.status_queue.put("Set your OpenAPI API Key in Settings and Restart the App")
+            self.status_queue.put('Set your OpenAPI API Key in Settings and Restart the App')
 
     def execute_user_request(self, user_request: str) -> None:
         self.stop_previous_request()
@@ -41,41 +43,47 @@ class Core:
                 Also, it is needed because the LLM we are using doesn't have a stateful/assistant mode.
         """
         self.interrupt_execution = False
+
+        if not self.llm:
+            status = 'Set your OpenAPI API Key in Settings and Restart the App'
+            self.status_queue.put(status)
+            return status
+
         try:
             instructions: dict[str, Any] = self.llm.get_instructions_for_objective(user_request, step_num)
 
             if instructions == {}:
                 # Sometimes LLM sends malformed JSON response, in that case retry once more.
-                instructions = self.llm.get_instructions_for_objective(user_request + " Please reply in valid JSON",
+                instructions = self.llm.get_instructions_for_objective(user_request + ' Please reply in valid JSON',
                                                                        step_num)
 
-            for step in instructions["steps"]:
+            for step in instructions['steps']:
                 if self.interrupt_execution:
-                    self.status_queue.put("Interrupted")
+                    self.status_queue.put('Interrupted')
                     self.interrupt_execution = False
-                    return "Interrupted"
+                    return 'Interrupted'
 
                 success = self.interpreter.process_command(step)
 
                 if not success:
-                    return "Unable to execute the request"
+                    return 'Unable to execute the request'
 
         except Exception as e:
-            self.status_queue.put(f"Exception Unable to execute the request - {e}")
-            return f"Unable to execute the request - {e}"
+            status = f'Exception Unable to execute the request - {e}'
+            self.status_queue.put(status)
+            return status
 
-        if instructions["done"]:
+        if instructions['done']:
             # Communicate Results
-            self.status_queue.put(instructions["done"])
+            self.status_queue.put(instructions['done'])
             self.play_ding_on_completion()
-
-            return instructions["done"]
+            return instructions['done']
         else:
             # if not done, continue to next phase
-            self.status_queue.put("Fetching further instructions based on current state")
+            self.status_queue.put('Fetching further instructions based on current state')
             return self.execute(user_request, step_num + 1)
 
     def play_ding_on_completion(self):
         # Play ding sound to signal completion
-        if 'play_ding_on_completion' in self.settings_dict and self.settings_dict['play_ding_on_completion']:
+        if self.settings_dict.get('play_ding_on_completion'):
             print('\a')
