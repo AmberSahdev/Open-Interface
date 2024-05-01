08f879738e658148ca03357f0d1b8707758dd538
Keshav Hari <keshav.harisrikanth@gmail.com>
2024-05-01 17:06:15 -0400

    minor comment change

--- app/ui.py
+++ app/ui.py
@@ -149,7 +149,7 @@ class UI:
             link_label.bind('<Button-1>', lambda e: open_link(
                 'https://github.com/AmberSahdev/Open-Interface?tab=readme-ov-file#setup-%EF%B8%8F'))
             
-            # Check for Updates Label
+            # Check for updates Label
             update_label = tk.Label(self, text='Check for Updates', fg='#499CE4', font=('Helvetica', 10))
             update_label.pack()
             update_label.bind('<Button-1>', lambda e: open_link(
