2d68ea48060221d544df1ba840f141debfdad3ab
Amber Sahdev <asahdev0@gmail.com>
2024-11-08 19:11:02 -0800

    Fixed error that was not allowing llama/llava and other models

--- app/models/factory.py
+++ app/models/factory.py
@@ -5,9 +5,13 @@ from models.gpt4v import GPT4v
 class ModelFactory:
     @staticmethod
     def create_model(model_name, *args):
-        if model_name == 'gpt-4o':
-            return GPT4o(model_name, *args)
-        elif model_name == 'gpt-4-vision-preview' or model_name == 'gpt-4-turbo':
-            return GPT4v(model_name, *args)
-        else:
-            raise ValueError(f'Unsupported model type {model_name}. Create entry in app/models/')
+        try:
+            if model_name == 'gpt-4o':
+                return GPT4o(model_name, *args)
+            elif model_name == 'gpt-4-vision-preview' or model_name == 'gpt-4-turbo':
+                return GPT4v(model_name, *args)
+            else:
+                # Llama/Llava models will work with the standard code I wrote for GPT4V without the assitant mode features of gpt4o
+                return GPT4v(model_name, *args)
+        except Exception as e:
+            raise ValueError(f'Unsupported model type {model_name}. Create entry in app/models/. Error: {e}')
